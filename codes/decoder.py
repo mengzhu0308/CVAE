5f8322f0eae9bbd78638cff3e9650d3dbebb0294
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-01-20 18:21:31 +0800

    Update decoder.py

--- codes/decoder.py
+++ codes/decoder.py
@@ -13,13 +13,11 @@ def decoder(x):
     x = Reshape((7, 7, 128))(x)
 
     x = UpSampling2D()(x)
-    x = Conv2D(64, 3, padding='same', use_bias=False)(x)
-    x = BatchNormalization()(x)
+    x = Conv2D(64, 3, padding='same')(x)
     x = ReLU()(x)
 
     x = UpSampling2D()(x)
-    x = Conv2D(32, 3, padding='same', use_bias=False)(x)
-    x = BatchNormalization()(x)
+    x = Conv2D(32, 3, padding='same')(x)
     x = ReLU()(x)
 
     x = Conv2D(1, 3, padding='same')(x)
